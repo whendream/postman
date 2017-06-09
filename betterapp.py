065e630e2d2cb3ba970f3a3e03d300e58ac52dd0
jwen <jwen@jwen>
2017-06-10 01:46:55 +0800

    update

--- betterapp.py
+++ betterapp.py
@@ -1,4 +1,3 @@
-#coding=utf-8
 '''
 Created on 2017年6月5日
 
@@ -6,17 +5,30 @@ Created on 2017年6月5日
 '''
 from flask import Flask,render_template
 from flask_bootstrap import Bootstrap
-from myform import ReqFormV1
+from myform import ReqFormV1,SimpleForm
+from getResponse import getReq
 
 app = Flask(__name__)
 app.config.from_object('config')
 Bootstrap(app)
 
-@app.route("/")
+@app.route("/", methods = ['GET', 'POST'])
 def index():
     form = ReqFormV1()
+    if form.validate_on_submit():
+        result = getReq(form.url.data)
+        print(form.method.data)
+        return render_template('index.html',form = form,result=result)
     return render_template("index.html",form = form)
 
+@app.route("/simple", methods = ['GET', 'POST'])
+def simple():
+    form = SimpleForm()
+    if form.validate_on_submit():
+        result = getReq(form.url.data)
+        print(form.method.data)
+        return render_template('simple.html',form = form,result=result)
+    return render_template("simple.html",form = form)
 
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
--- myform.py
+++ myform.py
@@ -1,14 +1,21 @@
 __author__ = 'jun.wen'
 
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,TextAreaField
+from wtforms import StringField, SubmitField, TextAreaField, SelectField
 from wtforms.validators import DataRequired
-
-class ReqForm(FlaskForm):
-    url = StringField('url', validators=[DataRequired()])
 
 
 class ReqFormV1(FlaskForm):
-    url = StringField('please enter the URL: ', validators=[DataRequired()])
-    submit =  SubmitField("Send")
-    result = TextAreaField("Result: ")
+    #method = SelectField(choices=[('wm-api', 'biz-api', "other")])
+    method = SelectField(u'Method', choices=[('get', 'GET'), ('post', 'POST'), ('delete', 'DELETE')])
+    url = StringField('Please enter the URL: ', validators=[DataRequired()])
+    reqBody = TextAreaField("Request Body: ",render_kw={'class':'text-body','rows':15})
+    submit = SubmitField("Send")
+    result = TextAreaField("Result: ",render_kw={'class':'text-body','rows':20})
+
+
+class SimpleForm(FlaskForm):
+    action = SelectField(u'请选择场景', choices=[('get', '根据wm_people_id查看自媒体人信息'), ('post', '根据article_id查看文章信息'), ('delete', 'TODO')])
+    keyword = StringField(u'请根据场景输入关键字段', validators=[DataRequired()])
+    submit = SubmitField("Send")
+    result = TextAreaField("Result: ",render_kw={'class':'text-body','rows':20})
